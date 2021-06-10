ade64059b3d07adf631b11c150da4802239c0b4b
Ale Ornelas <me@ale.world>
2021-06-10 14:45:06 -0500

    style(base): setup eslint

--- packages/base/src/string.test.ts
+++ packages/base/src/string.test.ts
@@ -1,16 +1,16 @@
 import './string'
 describe('String Helpers', () => {
   describe('remove', () => {
-    it('should remove pattern', () => expect("Hello World".remove(/Hello /)).toEqual('World'));
-  });
+    it('should remove pattern', () => expect('Hello World'.remove(/Hello /)).toEqual('World'))
+  })
   describe('squish', () => {
-    it('should remove spaces and trails', () => expect(" \n  foo\n\r \t bar \n".squish()).toEqual("foo bar"));
-  });
+    it('should remove spaces and trails', () => expect(' \n  foo\n\r \t bar \n'.squish()).toEqual('foo bar'))
+  })
 
   describe('truncate', () => {
-    it('should truncate with only lenght', () => expect("Oh dear! Oh dear! I shall be late!".truncate(20)).toEqual("Oh dear! Oh dear!..."));
-    it('should truncate with ommision', () => expect("Oh dear! Oh dear! I shall be late!".truncate(20, {omission: '&hellip;'})).toEqual("Oh dear! Oh &hellip;"));
-    it('should truncate with string separator', () => expect("Oh dear! Oh dear! I shall be late!".truncate(18, {separator: ' '})).toEqual("Oh dear! Oh..."));
-    it('should truncate with regex separator', () => expect("Oh dear! Oh dear! I shall be late!".truncate(18, {separator: /\s/})).toEqual("Oh dear! Oh..."));
-  });
-});
+    it('should truncate with only lenght', () => expect('Oh dear! Oh dear! I shall be late!'.truncate(20)).toEqual('Oh dear! Oh dear!...'))
+    it('should truncate with ommision', () => expect('Oh dear! Oh dear! I shall be late!'.truncate(20, { omission: '&hellip;' })).toEqual('Oh dear! Oh &hellip;'))
+    it('should truncate with string separator', () => expect('Oh dear! Oh dear! I shall be late!'.truncate(18, { separator: ' ' })).toEqual('Oh dear! Oh...'))
+    it('should truncate with regex separator', () => expect('Oh dear! Oh dear! I shall be late!'.truncate(18, { separator: /\s/ })).toEqual('Oh dear! Oh...'))
+  })
+})
--- packages/base/src/string.ts
+++ packages/base/src/string.ts
@@ -13,36 +13,36 @@ interface OmissionOptions {
 interface String {
   /**
    * The method remove will remove all occurrences of the pattern
-   * @param pattern 
+   * @param pattern
    */
-  remove(pattern: RegExp): string
+  remove: (pattern: RegExp) => string
   /**
    * The method squish strips leading and trailing whitespace, and substitutes runs of whitespace with a single space each
    */
-  squish(): string
+  squish: () => string
   /**
    * The method truncate returns a copy of its receiver truncated after a given length
-   * @param length 
-   * @param options 
+   * @param length
+   * @param options
    */
-  truncate(length: number, options?: OmissionOptions): string
+  truncate: (length: number, options?: OmissionOptions) => string
 }
 
-String.prototype.remove = function(pattern: RegExp) {
+String.prototype.remove = function (pattern: RegExp) {
   return this.replace(pattern, '')
 }
 
-String.prototype.squish = function() {
+String.prototype.squish = function () {
   return this.replace(/^\s+/, '').replace(/\s+$/, '').replace(/\s+/, ' ')
 }
 
-String.prototype.truncate = function(length: number, options?: OmissionOptions) {
-  const {omission = "...", separator} = options ?? {}
-  if(separator === undefined) {
-    return this.slice(0, length-omission.length) + omission
+String.prototype.truncate = function (length: number, options?: OmissionOptions) {
+  const { omission = '...', separator } = options ?? {}
+  if (separator === undefined) {
+    return this.slice(0, length - omission.length) + omission
   }
   const maxString = this.slice(0, length - omission.length)
-  if(typeof separator === 'string') {
+  if (typeof separator === 'string') {
     const lastPosition = maxString.lastIndexOf(separator)
     return maxString.slice(0, lastPosition) + omission
   } else {
