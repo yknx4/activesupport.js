42757ef53bedbacf24cfa29d5339a3af942de364
Ale Ornelas <me@ale.world>
2021-06-14 21:00:47 -0500

    feat(date): implement base date helpers

--- packages/date/src/date.test.ts
+++ packages/date/src/date.test.ts
@@ -1,5 +1,57 @@
+import {DateTime} from 'luxon'
 import './date'
 
 describe('Date Helpers', () => {
+  describe('beginningOfDay', () => {
+    it('should get beginning of day', () =>  {
+      const date = new Date().beginningOfDay()
+      expect(date.getSeconds()).toEqual(0)
+      expect(date.getHours()).toEqual(0)
+      expect(date.getMinutes()).toEqual(0)
+      expect(date.getMilliseconds()).toEqual(0)
+    });
+  })
+
+  describe('beginningOfHour', () => {
+    it('should get beginning of hour', () =>  {
+      const date = new Date().beginningOfHour()
+      expect(date.getHours()).not.toEqual(0)
+      expect(date.getSeconds()).toEqual(0)
+      expect(date.getMinutes()).toEqual(0)
+      expect(date.getMilliseconds()).toEqual(0)
+    });
+  })
+
+  describe('midnight', () => {
+    it('should get midnight', () =>  {
+      const tomorrow = DateTime.now().plus({day: 1})
+      const date = new Date().midnight()
+      expect(date.getSeconds()).toEqual(0)
+      expect(date.getHours()).toEqual(0)
+      expect(date.getMinutes()).toEqual(0)
+      expect(date.getMilliseconds()).toEqual(0)
+      expect(date.getDate()).toEqual(tomorrow.day)
+    });
+  })
+
+  describe('endOfDay', () => {
+    it('should get end of day', () =>  {
+      const date = new Date().endOfDay()
+      expect(date.getSeconds()).toEqual(59)
+      expect(date.getHours()).toEqual(23)
+      expect(date.getMinutes()).toEqual(59)
+      expect(date.getMilliseconds()).toEqual(999)
+    });
+  })
+
+  describe('endOfHour', () => {
+    it('should get end of hour', () =>  {
+      const date = new Date().endOfHour()
+      expect(date.getHours()).not.toEqual(0)
+      expect(date.getSeconds()).toEqual(59)
+      expect(date.getMinutes()).toEqual(59)
+      expect(date.getMilliseconds()).toEqual(999)
+    });
+  })
   
 })
--- packages/date/src/date.ts
+++ packages/date/src/date.ts
@@ -1,33 +1,40 @@
-import {DateTime} from 'luxon'
+import {DateTime, Duration} from 'luxon'
 
 declare global {
+ 
   interface Date {
     beginningOfDay(): Date
     beginningOfHour(): Date
     midnight(): Date
     endOfDay(): Date
     endOfHour(): Date
+    ago(input: Duration): Date
+    fromNow(input: Duration): Date
     since(seconds: number): Date
     in(seconds: number): Date
+    utc(): Date
+    secondsSinceMidnight (): number
+    advance(option: object): Date
+    change(option: object): Date
   }
 }
 
 Date.prototype.beginningOfDay = function() {
-  throw new Error('Not Implemented')
+  return DateTime.fromJSDate(this).set({hour: 0, minute: 0, second: 0, millisecond: 0}).toJSDate()
 }
 
 Date.prototype.beginningOfHour = function() {
-  throw new Error('Not Implemented')
+  return DateTime.fromJSDate(this).set({minute: 0, second: 0, millisecond: 0}).toJSDate()
 }
 
 Date.prototype.midnight = function() {
-  throw new Error('Not Implemented')
+  return DateTime.fromJSDate(this).set({hour: 24, minute: 0, second: 0, millisecond: 0}).toJSDate()
 }
 
 Date.prototype.endOfDay = function() {
-  throw new Error('Not Implemented')
+  return DateTime.fromJSDate(this).set({hour: 23, minute: 59, second: 59, millisecond: 999}).toJSDate()
 }
 
 Date.prototype.endOfHour = function() {
-  throw new Error('Not Implemented')
+  return DateTime.fromJSDate(this).set({minute: 59, second: 59, millisecond: 999}).toJSDate()
 }
